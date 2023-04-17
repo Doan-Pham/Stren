62aea24c85d35bd37c9619dd11033ddf6ba9e937
DoanPham <doannphamm57@gmail.com>
2023-04-17 09:45:49 +0700

    fix: SplashScreen keeps showing after opening app
    SplashScreen is supposed to show only the first time opening app, then
    pressing back button and reopening should not show SplashScreen. This is
    due to SplashScreen implementation as a static composable
    
    Solution: Migrate to SplashScreen API recommended by Google which has
    the correct behavior

--- app/src/main/java/com/example/stren/MainActivity.kt
+++ app/src/main/java/com/example/stren/MainActivity.kt
@@ -5,12 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.stren.ui.theme.StrenTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             StrenTheme {
