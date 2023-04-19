88d11af7f0ff357c8946e1df1f298481b688274f
DoanPham <doannphamm57@gmail.com>
2023-04-19 14:06:41 +0700

    refactor: Extract NavHost into its own composable
    For encapsulating and unit testing NavHost

--- app/src/main/java/com/haidoan/android/stren/app/StrenApp.kt
+++ app/src/main/java/com/haidoan/android/stren/app/StrenApp.kt
@@ -1,25 +1,13 @@
 package com.haidoan.android.stren.app
 
-import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.google.accompanist.navigation.animation.AnimatedNavHost
-import com.google.accompanist.navigation.animation.composable
 import com.haidoan.android.stren.designsystem.component.BottomNavigationBar
-import com.haidoan.android.stren.navigation.NAV_ROUTE_AUTH
-import com.haidoan.android.stren.navigation.TopLevelDestination
-import com.haidoan.android.stren.navigation.authenticationGraph
 
 private const val TAG = "StrenApp"
 val LocalSnackbarHostState =
@@ -28,7 +16,6 @@ val LocalSnackbarHostState =
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun StrenApp(
-    modifier: Modifier,
     viewModel: StrenAppViewModel = hiltViewModel(),
     appState: StrenAppState = rememberStrenAppState()
 ) {
@@ -47,99 +34,7 @@ fun StrenApp(
                 }
             }
         ) {
-            AnimatedNavHost(
-                navController = appState.navController,
-                startDestination = NAV_ROUTE_AUTH,
-                modifier = modifier.fillMaxSize(),
-                enterTransition = {
-                    slideIntoContainer(
-                        AnimatedContentScope.SlideDirection.Left,
-                        animationSpec = tween(700)
-                    )
-                },
-                popEnterTransition = {
-                    slideIntoContainer(
-                        AnimatedContentScope.SlideDirection.Left,
-                        animationSpec = tween(700)
-                    )
-                },
-                popExitTransition = {
-                    slideOutOfContainer(
-                        AnimatedContentScope.SlideDirection.Left,
-                        animationSpec = tween(700)
-                    )
-                },
-                exitTransition = {
-                    slideOutOfContainer(
-                        AnimatedContentScope.SlideDirection.Left,
-                        animationSpec = tween(700)
-                    )
-                }
-            ) {
-                authenticationGraph(
-                    appState.navController,
-                    onUserAlreadySignedIn = {
-                        if (isUserSignedIn) {
-                            appState.navController.navigate(TopLevelDestination.DASHBOARD.route) {
-                                launchSingleTop = true
-                                popUpTo(NAV_ROUTE_AUTH) { inclusive = true }
-                            }
-                        }
-                    },
-                )
-                composable(route = TopLevelDestination.DASHBOARD.route) {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .background(color = Color.Black)
-                    )
-                    if (!isUserSignedIn) {
-                        appState.navController.navigate(NAV_ROUTE_AUTH) {
-                            launchSingleTop = true
-                            popUpTo("Test") { inclusive = true }
-                        }
-                    }
-                }
-                composable(route = TopLevelDestination.TRAINING.route) {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .background(color = Color.Red)
-                    )
-                    if (!isUserSignedIn) {
-                        appState.navController.navigate(NAV_ROUTE_AUTH) {
-                            launchSingleTop = true
-                            popUpTo("Test") { inclusive = true }
-                        }
-                    }
-                }
-                composable(route = TopLevelDestination.NUTRITION.route) {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .background(color = Color.Yellow)
-                    )
-                    if (!isUserSignedIn) {
-                        appState.navController.navigate(NAV_ROUTE_AUTH) {
-                            launchSingleTop = true
-                            popUpTo("Test") { inclusive = true }
-                        }
-                    }
-                }
-                composable(route = TopLevelDestination.PROFILE.route) {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .background(color = Color.Green)
-                    )
-                    if (!isUserSignedIn) {
-                        appState.navController.navigate(NAV_ROUTE_AUTH) {
-                            launchSingleTop = true
-                            popUpTo("Test") { inclusive = true }
-                        }
-                    }
-                }
-            }
+            StrenNavHost(navController = appState.navController, isUserSignedIn = isUserSignedIn)
         }
     }
 }
