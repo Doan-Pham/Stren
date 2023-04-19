7a54186c691f9dd81158f6038fe53e44b85c51a8
DoanPham <doannphamm57@gmail.com>
2023-04-19 13:35:48 +0700

    feat: Move some state out of StrenApp
    For better encapsulation

---
+++ app/src/main/java/com/haidoan/android/stren/app/StrenAppState.kt
@@ -0,0 +1,38 @@
+package com.haidoan.android.stren.app
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
+import com.haidoan.android.stren.navigation.TopLevelDestination
+
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun rememberStrenAppState(
+    navController: NavHostController = rememberAnimatedNavController(),
+): StrenAppState {
+    return remember(navController) { StrenAppState(navController) }
+}
+
+class StrenAppState(val navController: NavHostController) {
+
+    val currentDestination: NavDestination?
+        @Composable get() = navController
+            .currentBackStackEntryAsState().value?.destination
+
+    val currentTopLevelDestination: TopLevelDestination?
+        @Composable get() = when (currentDestination?.route) {
+            TopLevelDestination.DASHBOARD.route -> TopLevelDestination.DASHBOARD
+            TopLevelDestination.TRAINING.route -> TopLevelDestination.TRAINING
+            TopLevelDestination.NUTRITION.route -> TopLevelDestination.NUTRITION
+            TopLevelDestination.PROFILE.route -> TopLevelDestination.PROFILE
+            else -> null
+        }
+
+    val shouldShowBottomBar: Boolean
+        @Composable get() = currentTopLevelDestination != null
+}
