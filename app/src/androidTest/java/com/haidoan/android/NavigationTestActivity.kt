b665f05bc375b222c43d83af2ebbecdd607574b0
DoanPham <doannphamm57@gmail.com>
2023-04-21 12:30:37 +0700

    test: Add NavigationTestActivity to hold NavHost
    This activity holds StrenApp and has methods for direct state
    manipulation for testing purposes

---
+++ app/src/androidTest/java/com/haidoan/android/NavigationTestActivity.kt
@@ -0,0 +1,48 @@
+package com.haidoan.android
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.LocalActivityResultRegistryOwner
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.facebook.CallbackManager
+import com.haidoan.android.stren.app.LocalFacebookCallbackManager
+import com.haidoan.android.stren.app.StrenApp
+import com.haidoan.android.stren.app.StrenAppViewModel
+import com.haidoan.android.stren.core.service.FakeAuthenticationServiceImpl
+import com.haidoan.android.stren.designsystem.theme.StrenTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+private const val TAG = "NavigationTestActivity"
+
+@AndroidEntryPoint
+class NavigationTestActivity : ComponentActivity() {
+    private var facebookCallbackManager = CallbackManager.Factory.create()
+    var isUserSignedIn by mutableStateOf(false)
+    private val fakeAuthenticationServiceImpl = FakeAuthenticationServiceImpl()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            StrenTheme {
+                CompositionLocalProvider(
+                    LocalFacebookCallbackManager provides facebookCallbackManager,
+                    LocalActivityResultRegistryOwner provides this
+                ) {
+                    Log.d(TAG, "isUserSignedIn: $isUserSignedIn")
+                    fakeAuthenticationServiceImpl.isUserSignedIn = isUserSignedIn
+
+                    StrenApp(viewModel = StrenAppViewModel(fakeAuthenticationServiceImpl))
+                }
+            }
+        }
+    }
+}
