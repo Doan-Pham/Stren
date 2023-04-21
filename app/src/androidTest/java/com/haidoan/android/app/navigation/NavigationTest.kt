e5c8aa417831f5a5663d51c104edde0bc0dfb644
DoanPham <doannphamm57@gmail.com>
2023-04-21 13:47:14 +0700

    test: Add FakeAuthenticationServiceImpl for tests
    viewModel in StrenApp is of type StrenAppViewModel and injected using
    hiltViewModel(), so creating and injecting fake ViewModel class will be
     tedious.
    
    Instead, a FakeAuthenticationServiceImpl that's injected into the
    viewModel using constructor and factory is more simple.
    
    The downside is that some composables lower in the hierarchy may use
    AuthenticationServiceImpl so it's currently impossible to test some
    navigation flows that involve them.

--- app/src/androidTest/java/com/haidoan/android/app/navigation/NavigationTest.kt
+++ app/src/androidTest/java/com/haidoan/android/app/navigation/NavigationTest.kt
@@ -1,16 +1,21 @@
 package com.haidoan.android.app.navigation
 
 import android.content.Context
-import androidx.compose.ui.test.*
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.ComposeContentTestRule
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onAllNodesWithTag
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
 import androidx.navigation.testing.TestNavHostController
 import androidx.test.platform.app.InstrumentationRegistry
+import com.haidoan.android.NavigationTestActivity
 import com.haidoan.android.stren.R
-import com.haidoan.android.stren.app.MainActivity
 import com.haidoan.android.stren.designsystem.component.TEST_TAG_BOTTOM_NAV
+import com.haidoan.android.stren.feat.auth.login.TEST_TAG_SCREEN_LOGIN
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import java.util.*
@@ -24,39 +29,53 @@ class NavigationTest {
 
     @get:Rule(order = 1)
 
-    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    val composeTestRule = createAndroidComposeRule<NavigationTestActivity>()
     lateinit var navController: TestNavHostController
 
-    val context: Context = InstrumentationRegistry.getInstrumentation().getTargetContext()
+    private val context: Context = InstrumentationRegistry.getInstrumentation().targetContext
     val dashboardLabel = context.resources.getString(R.string.bottom_nav_title_dashboard)
     val trainingLabel = context.resources.getString(R.string.bottom_nav_title_training)
     val nutritionLabel = context.resources.getString(R.string.bottom_nav_title_nutrition)
     val profileLabel = context.resources.getString(R.string.bottom_nav_title_profile)
 
+
+    @Before
+    fun setupStrenNavHost() {
+        composeTestRule.activity.isUserSignedIn = false
+    }
+
+
     @Test
-    fun strenNavHost_loginSuccess_dashBoardScreenIsShown() {
+    fun strenNavHost_userNotSignedIn_loginScreenIsShown() {
         composeTestRule.apply {
-            onNodeWithText("Email").performTextInput("asd@gmail.com")
-            onNodeWithText("Password").performTextInput("asdqwe")
-            onNodeWithText("Login").performClick()
-            waitUntilTimeout(5000L)
+            onNodeWithTag(TEST_TAG_SCREEN_LOGIN).assertIsDisplayed()
+            onNodeWithText("Email").assertIsDisplayed()
+            onNodeWithText("Password").assertIsDisplayed()
+            onNodeWithText("Login").assertIsDisplayed()
+        }
+    }
+
+    @Test
+    fun strenNavHost_userAlreadySignedIn_dashboardScreenIsShown() {
+        composeTestRule.apply {
+            activity.isUserSignedIn = true
+            waitUntilTimeout(4000L)
             onNodeWithTag("Screen-Dashboard").assertIsDisplayed()
             onNodeWithTag(TEST_TAG_BOTTOM_NAV).assertIsDisplayed()
         }
     }
 
     @Test
-    fun strenNavHost_verifyStartDestination() {
+    fun strenNavHost_userSignOut_loginScreenIsShown() {
         composeTestRule.apply {
-            onNodeWithText("Email").performTextInput("asd@gmail.com")
-            onNodeWithText("Password").performTextInput("asdqwe")
-            onNodeWithText("Login").performClick()
-            waitUntil(10000) {
-                onAllNodesWithTag(TEST_TAG_BOTTOM_NAV).fetchSemanticsNodes().size == 1
-            }
-            onNodeWithText(dashboardLabel).assertIsSelected()
+            activity.isUserSignedIn = true
+            waitUntil { onAllNodesWithTag(TEST_TAG_BOTTOM_NAV).fetchSemanticsNodes().size == 1 }
+            activity.isUserSignedIn = false
+            onNodeWithTag(TEST_TAG_SCREEN_LOGIN).assertIsDisplayed()
         }
     }
+
 }
 
 fun ComposeContentTestRule.waitUntilTimeout(
