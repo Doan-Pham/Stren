f30cdc296ba84f84021580ee9e3cd6a5cc74c779
DoanPham <doannphamm57@gmail.com>
2023-04-21 22:44:03 +0700

    refactor: Reorganize classes into package
    Application will now only have 3 root packages:
    - app: Everything that concerns the whole app (app navigation, app
    entry points, root composable...)
    - feat: Application's features, with each immediate sub-package
    encapsulating to a feature group
    - core: Classes and composables that are used across different features
     (repositories,models, services, utils, UI components...)
    
    Reference: https://github.com/android/nowinandroid/blob/main/docs/ModularizationLearningJourney.md

--- app/src/main/java/com/haidoan/android/stren/navigation/Screen.kt
+++
@@ -1,6 +0,0 @@
-package com.haidoan.android.stren.navigation
-
-sealed class Screen(val route: String) {
-    object Login : Screen(route = "login_screen")
-    object Signup : Screen(route = "signup_screen")
-}
