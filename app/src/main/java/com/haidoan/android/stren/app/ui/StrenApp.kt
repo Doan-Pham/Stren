8f460fe3a8c675c084b0f1e1a2ad30602db5351c
DoanPham <doannphamm57@gmail.com>
2023-04-23 22:10:31 +0700

    feat(UI): Add composable StrenTopAppBar

--- app/src/main/java/com/haidoan/android/stren/app/ui/StrenApp.kt
+++ app/src/main/java/com/haidoan/android/stren/app/ui/StrenApp.kt
@@ -1,4 +1,4 @@
-package com.haidoan.android.stren.app
+package com.haidoan.android.stren.app.ui
 
 import android.util.Log
 import androidx.compose.material3.Scaffold
@@ -6,11 +6,16 @@ import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.haidoan.android.stren.R
+import com.haidoan.android.stren.app.StrenAppViewModel
 import com.haidoan.android.stren.app.navigation.StrenNavHost
-import com.haidoan.android.stren.app.ui.StrenAppState
-import com.haidoan.android.stren.app.ui.rememberStrenAppState
 import com.haidoan.android.stren.core.designsystem.component.BottomNavigationBar
+import com.haidoan.android.stren.core.designsystem.component.StrenTopAppBar
+import com.haidoan.android.stren.core.designsystem.component.TEST_TAG_TOP_BAR
 
 private const val TAG = "StrenApp"
 val LocalSnackbarHostState =
@@ -31,6 +36,14 @@ fun StrenApp(
     ) {
         Scaffold(
             snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+            topBar = {
+                if (appState.shouldShowTopBar) {
+                    StrenTopAppBar(
+                        modifier = Modifier.testTag(TEST_TAG_TOP_BAR),
+                        title = stringResource(id = R.string.app_name)
+                    )
+                }
+            },
             bottomBar = {
                 if (appState.shouldShowBottomBar) {
                     BottomNavigationBar(
