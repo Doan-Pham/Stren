dca55cc83fb80da670083c12a9cd972bf58ea7d0
DoanPham <doannphamm57@gmail.com>
2023-04-24 13:06:39 +0700

    feat(UI):Add HorizontalPager in TrainingTabsScreen
    This allows for swiping between tabs in TrainingTabsScreen

--- app/src/main/java/com/haidoan/android/stren/feat/trainining/TrainingTabsScreen.kt
+++ app/src/main/java/com/haidoan/android/stren/feat/trainining/TrainingTabsScreen.kt
@@ -1,17 +1,28 @@
 package com.haidoan.android.stren.feat.trainining
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.*
 import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.haidoan.android.stren.core.designsystem.theme.Gray90
+import kotlinx.coroutines.launch
 
+private const val TAG = "TrainingTabsScreen"
+
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun TrainingTabsScreen(tabNamesAndScreenComposables: List<Pair<String, Unit>>) {
-    var tabIndex by remember { mutableStateOf(0) }
+    // on below line we are creating variable for pager state.
+    val pagerState = rememberPagerState(initialPage = 0)
+    val tabIndex = pagerState.currentPage
+    val coroutineScope = rememberCoroutineScope()
     Column(modifier = Modifier.fillMaxWidth()) {
         TabRow(selectedTabIndex = tabIndex, containerColor = Color.White, indicator = {
             TabRowDefaults.Indicator(
@@ -29,13 +40,24 @@ fun TrainingTabsScreen(tabNamesAndScreenComposables: List<Pair<String, Unit>>) {
                         )
                     },
                     selected = tabIndex == index,
-                    onClick = { tabIndex = index },
+                    onClick = {
+                        coroutineScope.launch {
+                            pagerState.animateScrollToPage(index)
+                        }
+                    },
                     selectedContentColor = MaterialTheme.colorScheme.primary,
                     unselectedContentColor = Gray90
                 )
             }
         }
-        tabNamesAndScreenComposables[tabIndex].second
+
+        HorizontalPager(
+            modifier = Modifier.fillMaxSize(),
+            state = pagerState,
+            pageCount = tabNamesAndScreenComposables.size
+        ) { page ->
+            tabNamesAndScreenComposables[page].second
+        }
     }
 
 }
