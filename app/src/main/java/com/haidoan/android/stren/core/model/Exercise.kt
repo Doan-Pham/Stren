4a12bcc24730fee7af2a7bb9d8d15e0f6b8590ff
DoanPham <doannphamm57@gmail.com>
2023-04-24 13:05:39 +0700

    feat(UI): Add ExerciseItem in ExercisesScreen
    Each item represents an exercise (name, photo, category)

--- app/src/main/java/com/haidoan/android/stren/core/model/Exercise.kt
+++ app/src/main/java/com/haidoan/android/stren/core/model/Exercise.kt
@@ -2,8 +2,8 @@ package com.haidoan.android.stren.core.model
 
 data class Exercise(
     val name: String,
-    val instruction: String,
-    val imageUrl: String,
-    val belongedCategories: List<String>,
-    val trainedMuscleGroups: List<String>
+    val instruction: String = "",
+    val imageUrl: String = "",
+    val belongedCategories: List<String> = listOf(),
+    val trainedMuscleGroups: List<String> = listOf()
 )
