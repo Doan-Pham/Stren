25ee94b8b50e9f9b7233ce3c5fdb9ba134e3430c
DoanPham <doannphamm57@gmail.com>
2023-04-25 22:12:52 +0700

    test: UI state is loaded in ExercisesScreen
    When UI state is loaded in ExercisesScreen, exercise items should be
    shown

--- app/src/androidTest/java/com/haidoan/android/feat/training/ExercisesScreenTest.kt
+++ app/src/androidTest/java/com/haidoan/android/feat/training/ExercisesScreenTest.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import com.haidoan.android.stren.core.testing.data.exercisesTestData
 import com.haidoan.android.stren.feat.trainining.exercises.EXERCISES_LOADING_ANIMATION_TEST_TAG
 import com.haidoan.android.stren.feat.trainining.exercises.ExercisesScreen
 import com.haidoan.android.stren.feat.trainining.exercises.ExercisesUiState
@@ -24,5 +26,19 @@ class ExercisesScreenTest {
         }
         composeTestRule.onNodeWithTag(EXERCISES_LOADING_ANIMATION_TEST_TAG).assertExists()
         composeTestRule.onNodeWithTag(EXERCISES_LOADING_ANIMATION_TEST_TAG).assertIsDisplayed()
+    }
+
+    @Test
+    fun exercisesScreen_uiStateLoadComplete_exercisesAreShown() {
+        composeTestRule.setContent {
+            BoxWithConstraints {
+                ExercisesScreen(uiState = ExercisesUiState.LoadComplete(exercisesTestData))
+            }
+        }
+
+        exercisesTestData.forEach {
+            composeTestRule.onNodeWithText(it.name).assertExists()
+            composeTestRule.onNodeWithText(it.trainedMuscleGroups.first()).assertExists()
+        }
     }
 }
