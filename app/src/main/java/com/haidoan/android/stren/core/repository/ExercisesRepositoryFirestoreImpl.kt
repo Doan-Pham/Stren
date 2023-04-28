f996e9be6b7c31046ee48b687fc84c9307350e99
DoanPham <doannphamm57@gmail.com>
2023-04-28 09:49:31 +0700

    fix: Can't convert Firestore data to model
    Description: Calling toObjects() on Firestore collection snapshots()
    causes app crash with a single error message: Channel is unrecoverably
    broken and will be disposed!
    
    Cause:
    - Firestore uses cache for mobile applications by default, which
    can lead to obsolete cache being read. If the collection has new fields
    or has modified its fields, the obsolete cache may return null for
    certain fields since the cache doesn't have these fields
    - toObjects() method can't convert Firestore array type to Kotlin List
    
    Solution:
    -Disable Firestore cache (which is temporary fix since this will
    lead more reads and slower performance)
    -Explicitly reading and casting the Firestore array fields to Kotlin
    List instead of using toObjects() method
    
    Reference:
    https://stackoverflow.com/questions/43011085/is-there-a-way-to-only-download-data-from-firebase-if-the-cached-data-is-outdate
    https://stackoverflow.com/questions/63266933/how-to-read-array-data-from-the-firestore-using-kotlin

--- app/src/main/java/com/haidoan/android/stren/core/repository/ExercisesRepositoryFirestoreImpl.kt
+++ app/src/main/java/com/haidoan/android/stren/core/repository/ExercisesRepositoryFirestoreImpl.kt
@@ -1,21 +1,41 @@
 package com.haidoan.android.stren.core.repository
 
+import com.google.firebase.firestore.FirebaseFirestoreSettings
+import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.snapshots
 import com.google.firebase.ktx.Firebase
 import com.haidoan.android.stren.core.model.Exercise
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.mapNotNull
 import javax.inject.Inject
 
 private const val EXERCISE_COLLECTION_PATH = "Exercise"
 
 class ExercisesRepositoryFirestoreImpl @Inject constructor() :
     ExercisesRepository {
+
     private val firestore = Firebase.firestore
-    private val exerciseCollection = firestore.collection(EXERCISE_COLLECTION_PATH)
+
+    init {
+        firestore.firestoreSettings =
+            FirebaseFirestoreSettings.Builder().setPersistenceEnabled(false).build()
+    }
 
     override fun getAllExercisesStream(): Flow<List<Exercise>> =
-        exerciseCollection.snapshots().map { it.toObjects(Exercise::class.java) }
+        firestore.collection(EXERCISE_COLLECTION_PATH).snapshots().toExerciseList()
 
+    private fun Flow<QuerySnapshot>.toExerciseList() = this.mapNotNull {
+        it.documents.mapNotNull { document ->
+            @Suppress("UNCHECKED_CAST")
+            Exercise(
+                document.id,
+                document.getString("name") ?: "",
+                document.getString("instruction") ?: "",
+                document.getString("imageUrl") ?: "",
+                document.get("belongedCategories") as List<String>,
+                document.get("trainedMuscleGroups") as List<String>
+            )
+        }
+    }
 }
