52144341e16272157203a5673efd42f1df89df59
DoanPham <doannphamm57@gmail.com>
2023-04-28 10:27:32 +0700

    test: Add UiState test for ExercisesViewModel
    Added tests:
    - uiState_initialValue_isLoading
    - uiState_exercisesLoaded_isLoadComplete

---
+++ app/src/test/java/com/haidoan/android/stren/feat/training/exercises/ExercisesViewModelTest.kt
@@ -0,0 +1,47 @@
+package com.haidoan.android.stren.feat.training.exercises
+
+import com.haidoan.android.stren.core.repository.fake.FakeExercisesRepository
+import com.haidoan.android.stren.core.testing.data.exercisesTestData
+import com.haidoan.android.stren.feat.trainining.exercises.ExercisesUiState
+import com.haidoan.android.stren.feat.trainining.exercises.ExercisesViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.*
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class ExercisesViewModelTest {
+
+    private lateinit var viewModel: ExercisesViewModel
+    private lateinit var fakeExercisesRepository: FakeExercisesRepository
+
+    @Before
+    fun setUp() {
+        fakeExercisesRepository = FakeExercisesRepository()
+        viewModel = ExercisesViewModel(fakeExercisesRepository)
+    }
+
+    @Test
+    fun uiState_initialValue_isLoading() = runTest {
+        assertEquals(ExercisesUiState.Loading, viewModel.uiState.value)
+    }
+
+    @Test
+    fun uiState_exercisesLoaded_isLoadComplete() = runTest {
+        // Need this since StateFlow created with stateIn doesn't update unless there's collectors
+        val collectJob =
+            launch(UnconfinedTestDispatcher(testScheduler)) { viewModel.uiState.collect() }
+        runCurrent()
+        fakeExercisesRepository.setExercises(exercisesTestData)
+        assertEquals(ExercisesUiState.LoadComplete(exercisesTestData), viewModel.uiState.value)
+
+        fakeExercisesRepository.setExercises(emptyList())
+        assertEquals(ExercisesUiState.LoadComplete(emptyList()), viewModel.uiState.value)
+
+        collectJob.cancel()
+    }
+}
