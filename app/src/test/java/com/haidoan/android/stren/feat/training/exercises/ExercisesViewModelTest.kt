09e962cc91cc9a3ed9d28156c1c4863a2f42be87
DoanPham <doannphamm57@gmail.com>
2023-04-28 10:42:37 +0700

    fix: ExerciseUiState test returns false result
    Context: Testing when exercises are loaded, ExerciseUiState in
    ExercisesViewModel changes to LoadedComplete with data inside.
    
    Description: The uiState stays unchanged as the initial value, even
    when repository loads new data
    
    Cause: viewModelScope API in ViewModel classes uses a hardcoded Main
    dispatcher that wraps around UI thread, which is not available during
    local unit tests. Therefore, the coroutines in ExercisesViewModel run
    unpredictably leading to assertion failures, since results are asserted
    before they are processed
    
    Fix: Replace the Main dispatcher in ViewModel with TestDispatcher
    which is specialized for testing
    
    Reference:
    https://stackoverflow.com/questions/72746719/unit-testing-android-viewmodel-with-a-stateflow-that-mapslatest-from-another-sta
    https://developer.android.com/kotlin/coroutines/test#setting-main-dispatcher

--- app/src/test/java/com/haidoan/android/stren/feat/training/exercises/ExercisesViewModelTest.kt
+++ app/src/test/java/com/haidoan/android/stren/feat/training/exercises/ExercisesViewModelTest.kt
@@ -2,6 +2,7 @@ package com.haidoan.android.stren.feat.training.exercises
 
 import com.haidoan.android.stren.core.repository.fake.FakeExercisesRepository
 import com.haidoan.android.stren.core.testing.data.exercisesTestData
+import com.haidoan.android.stren.core.testing.util.MainDispatcherRule
 import com.haidoan.android.stren.feat.trainining.exercises.ExercisesUiState
 import com.haidoan.android.stren.feat.trainining.exercises.ExercisesViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -10,11 +11,14 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.*
 import org.junit.Assert.*
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class ExercisesViewModelTest {
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
 
     private lateinit var viewModel: ExercisesViewModel
     private lateinit var fakeExercisesRepository: FakeExercisesRepository
@@ -32,10 +36,9 @@ class ExercisesViewModelTest {
 
     @Test
     fun uiState_exercisesLoaded_isLoadComplete() = runTest {
-        // Need this since StateFlow created with stateIn doesn't update unless there's collectors
-        val collectJob =
-            launch(UnconfinedTestDispatcher(testScheduler)) { viewModel.uiState.collect() }
-        runCurrent()
+        // Need to manually collect flow since StateFlow created with stateIn doesn't update unless there's collectors
+        val collectJob = launch(UnconfinedTestDispatcher()) { viewModel.uiState.collect() }
+
         fakeExercisesRepository.setExercises(exercisesTestData)
         assertEquals(ExercisesUiState.LoadComplete(exercisesTestData), viewModel.uiState.value)
 
