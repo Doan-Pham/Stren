08fdc4c1ef3cbdc4868d4f39a96f88e279ec4375
DoanPham <doannphamm57@gmail.com>
2023-04-29 11:41:58 +0700

    fix: AppBar in ExercisesScreen doesn't show icons
    Description: The app bar is correctly configured when switching
    between tabs in TrainingTabsScreen. However, when navigating from
    another top-level destination to Training destination (which holds the
    TrainingTabsScreen), the action icons for ExercisesScreen (The first tab
    in TrainingTabsScreen) flash for a second before disappearing
    
    Cause: Each top-level destination configures the app bar whenever it's
    recomposed to keep the app bar up-to-date with the current screen.
    However, because navigation transition may include animations,
    in which case recomposition may happen frequently. And the following
    situation may happen: Currently at Destination A -> Navigate from
    Destination A to B -> Currently at Destination B so Destination B
    configures app bar -> Destination A gets recomposed since
    navigation animation is  not done -> Destination A configures app bar
    and overrides Destination B configuration.
    
    Solution: Makes sure each top-level destination only configures appbar
    once each time it enters the composition
    
    Reference: https://stackoverflow.com/questions/70615985/composable-in-navgraphbuilder-is-repeating-3-or-more-times
    
    TODO: Replace the current solution with Side Effects API, which is
    made for these situations

--- app/src/main/java/com/haidoan/android/stren/app/ui/StrenApp.kt
+++ app/src/main/java/com/haidoan/android/stren/app/ui/StrenApp.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.haidoan.android.stren.R
 import com.haidoan.android.stren.app.StrenAppViewModel
+import com.haidoan.android.stren.app.navigation.AppBarConfiguration
 import com.haidoan.android.stren.app.navigation.StrenNavHost
 import com.haidoan.android.stren.core.designsystem.component.BottomNavigationBar
 import com.haidoan.android.stren.core.designsystem.component.StrenTopAppBar
@@ -29,8 +30,12 @@ fun StrenApp(
 ) {
     val snackbarHostState = remember { SnackbarHostState() }
     val isUserSignedIn by rememberSaveable { viewModel.isUserSignedIn }
+    var currentTopAppBarConfiguration by remember {
+        mutableStateOf(AppBarConfiguration())
+    }
 
     Log.d(TAG, "isUserSignedIn: $isUserSignedIn")
+    Log.d(TAG, "currentTopAppBarConfiguration: ${currentTopAppBarConfiguration}")
     // This allows any screen in the composition to access snackbar
     CompositionLocalProvider(
         LocalSnackbarHostState provides snackbarHostState
@@ -42,7 +47,7 @@ fun StrenApp(
                     StrenTopAppBar(
                         modifier = Modifier.testTag(TEST_TAG_TOP_BAR),
                         title = stringResource(id = R.string.app_name),
-                        appBarConfiguration = appState.currentTopAppBarConfiguration
+                        appBarConfiguration = currentTopAppBarConfiguration
                     )
                 }
             },
@@ -61,7 +66,7 @@ fun StrenApp(
                 navController = appState.navController,
                 isUserSignedIn = isUserSignedIn,
                 appBarConfigurationChangeHandler = { newConfiguration ->
-                    appState.currentTopAppBarConfiguration = newConfiguration
+                    currentTopAppBarConfiguration = newConfiguration
                 }
             )
         }
