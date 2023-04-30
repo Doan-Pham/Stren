ffde9eca9c7182ffae1bb8c40695d3dc9fb81b63
DoanPham <doannphamm57@gmail.com>
2023-04-30 14:03:37 +0700

    test: Rewrite tests for ExercisesScreen, ViewModel
    ExercisesScreen and ExercisesViewModel now use Paging 3 to load data

--- app/src/androidTest/java/com/haidoan/android/feat/training/ExercisesScreenTest.kt
+++ app/src/androidTest/java/com/haidoan/android/feat/training/ExercisesScreenTest.kt
@@ -2,14 +2,13 @@ package com.haidoan.android.feat.training
 
 import androidx.activity.ComponentActivity
 import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.paging.PagingData
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.haidoan.android.stren.core.testing.data.exercisesTestData
-import com.haidoan.android.stren.feat.trainining.exercises.EXERCISES_LOADING_ANIMATION_TEST_TAG
 import com.haidoan.android.stren.feat.trainining.exercises.ExercisesScreen
-import com.haidoan.android.stren.feat.trainining.exercises.ExercisesUiState
+import kotlinx.coroutines.flow.flowOf
 import org.junit.Rule
 import org.junit.Test
 
@@ -18,24 +17,14 @@ class ExercisesScreenTest {
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
     @Test
-    fun loadingAnimation_uiStateIsLoading_isShown() {
+    fun exercisesScreen_exercisesLoaded_exercisesAreShown() {
         composeTestRule.setContent {
             BoxWithConstraints {
-                ExercisesScreen(uiState = ExercisesUiState.Loading)
+                val exercises =
+                    flowOf(PagingData.from(exercisesTestData)).collectAsLazyPagingItems()
+                ExercisesScreen(pagedExercises = exercises)
             }
         }
-        composeTestRule.onNodeWithTag(EXERCISES_LOADING_ANIMATION_TEST_TAG).assertExists()
-        composeTestRule.onNodeWithTag(EXERCISES_LOADING_ANIMATION_TEST_TAG).assertIsDisplayed()
-    }
-
-    @Test
-    fun exercisesScreen_uiStateLoadComplete_exercisesAreShown() {
-        composeTestRule.setContent {
-            BoxWithConstraints {
-                ExercisesScreen(uiState = ExercisesUiState.LoadComplete(exercisesTestData))
-            }
-        }
-
         exercisesTestData.forEach {
             composeTestRule.onNodeWithText(it.name).assertExists()
             composeTestRule.onNodeWithText(it.trainedMuscleGroups.first()).assertExists()
