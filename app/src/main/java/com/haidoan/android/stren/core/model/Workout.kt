62f6c83933a0437123271ee20547d2ef9444b0bb
DoanPham <doannphamm57@gmail.com>
2023-05-10 11:15:03 +0700

    feat: Add toString() methods for Workout
    Each TrainingMeasurementMetrics has its own logic for displaying as
    String, which can be quite bulky

--- app/src/main/java/com/haidoan/android/stren/core/model/Workout.kt
+++ app/src/main/java/com/haidoan/android/stren/core/model/Workout.kt
@@ -1,6 +1,7 @@
 package com.haidoan.android.stren.core.model
 
 import com.google.firebase.firestore.DocumentId
+import java.text.DecimalFormat
 import java.time.LocalDate
 
 data class Workout(
@@ -18,8 +19,34 @@ data class TrainedExercise(
 )
 
 sealed interface TrainingMeasurementMetrics {
-    data class WeightAndRep(val weight: String, val repAmount: Long) : TrainingMeasurementMetrics
-    data class DurationOnly(val seconds: Long) : TrainingMeasurementMetrics
+    override fun toString(): String
+    data class WeightAndRep(val weight: String, val repAmount: Long) : TrainingMeasurementMetrics {
+        override fun toString(): String {
+            return "$weight x $repAmount"
+        }
+    }
+
+    data class DurationOnly(val seconds: Long) : TrainingMeasurementMetrics {
+        override fun toString(): String {
+            if (seconds > 3600) return "${seconds / 3600} hrs"
+            if (seconds > 60) return "${seconds / 60} mins"
+            return "${seconds}s"
+        }
+    }
+
     data class DistanceAndDuration(val kilometers: Long, val hours: Double) :
-        TrainingMeasurementMetrics
+        TrainingMeasurementMetrics {
+        override fun toString(): String {
+            val distance = if (kilometers < 1) "${kilometers * 1000}m" else "${kilometers}km"
+            val duration =
+                if (hours < (1 / 60)) {
+                    "${(hours * 3600).toLong()}s"
+                } else if (hours < 1) {
+                    "${(hours * 60).toLong()} mins"
+                } else {
+                    "${DecimalFormat("#.#").format(hours)} hrs"
+                }
+            return "$distance x $duration"
+        }
+    }
 }
