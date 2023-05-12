f8c55c702543fc03b466b6758741bf38e7ccda57
DoanPham <doannphamm57@gmail.com>
2023-05-12 08:40:35 +0700

    fix: Wrong data type in AddEditRoutineUiState
    The content in AddEditRoutineScreen should be TrainedExercise objects,
    not Routine ones. Only when the user click saves, a new Routine object
    is created

--- app/src/main/java/com/haidoan/android/stren/feat/trainining/routines/add_edit/AddEditRoutineUiState.kt
+++ app/src/main/java/com/haidoan/android/stren/feat/trainining/routines/add_edit/AddEditRoutineUiState.kt
@@ -1,9 +1,9 @@
 package com.haidoan.android.stren.feat.trainining.routines.add_edit
 
-import com.haidoan.android.stren.core.model.Routine
+import com.haidoan.android.stren.core.model.TrainedExercise
 
 internal sealed interface AddEditRoutineUiState {
     object Loading : AddEditRoutineUiState
     object EmptyRoutine : AddEditRoutineUiState
-    data class IsAdding(val routines: List<Routine>) : AddEditRoutineUiState
+    data class IsAdding(val trainedExercises: List<TrainedExercise>) : AddEditRoutineUiState
 }
