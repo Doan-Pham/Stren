9c85d8469bbea967079b1d533c1679edb4de8149
DoanPham <doannphamm57@gmail.com>
2023-05-13 11:13:56 +0700

    feat(UI): Add composables to represent exercises
    Each exercise in a routine is represented by the ExerciseRegion
    composable consisting of: exercise name, training sets, "Add set" button
    
    Training sets for each given exercise will be represented in a table
    layout, with the first column at fixed size, and the remaining columns
    dividing the remaining width between them. This is done by using
    an outer Column combined with the newly added TrainingSetRow composable
    
    Reference: https://stackoverflow.com/questions/70898479/how-can-i-align-widths-of-first-column-in-a-list-of-rows-using-jetpack-compose

--- app/src/main/java/com/haidoan/android/stren/feat/trainining/routines/add_edit/AddEditRoutineScreen.kt
+++ app/src/main/java/com/haidoan/android/stren/feat/trainining/routines/add_edit/AddEditRoutineScreen.kt
@@ -1,16 +1,23 @@
 package com.haidoan.android.stren.feat.trainining.routines.add_edit
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.haidoan.android.stren.R
@@ -19,6 +26,8 @@ import com.haidoan.android.stren.app.navigation.IconButtonInfo
 import com.haidoan.android.stren.core.designsystem.component.LoadingAnimation
 import com.haidoan.android.stren.core.designsystem.component.StrenFilledButton
 import com.haidoan.android.stren.core.designsystem.component.StrenOutlinedTextField
+import com.haidoan.android.stren.core.model.TrainedExercise
+import com.haidoan.android.stren.core.model.TrainingMeasurementMetrics
 import timber.log.Timber
 
 internal const val ADD_EDIT_ROUTINE_SCREEN_ROUTE = "add_edit_routine_screen_route"
@@ -83,8 +92,7 @@ internal fun AddEditRoutineScreen(
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Column(
-            modifier = Modifier.weight(1f),
-            horizontalAlignment = Alignment.CenterHorizontally
+            modifier = Modifier.weight(1f), horizontalAlignment = Alignment.CenterHorizontally
         ) {
             StrenOutlinedTextField(
                 text = routineName,
@@ -107,12 +115,18 @@ internal fun AddEditRoutineScreen(
                 is AddEditRoutineUiState.IsAdding -> {
                     Timber.d("IsAdding")
                     Timber.d("trainedExercises: ${uiState.trainedExercises}")
+                    LazyColumn {
+                        items(uiState.trainedExercises) { trainedExercise ->
+                            TrainedExerciseRegion(trainedExercise = trainedExercise)
+                        }
+                    }
                 }
             }
         }
 
         StrenFilledButton(
-            text = "Add exercise", onClickHandler = onNavigateToAddExercise,
+            text = "Add exercise",
+            onClickHandler = onNavigateToAddExercise,
             textStyle = MaterialTheme.typography.bodyMedium
         )
     }
@@ -121,16 +135,13 @@ internal fun AddEditRoutineScreen(
 
 @Composable
 private fun ColumnScope.EmptyScreen() {
-
     Column(
-        modifier = Modifier
-            .weight(1f),
+        modifier = Modifier.weight(1f),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Icon(
-            painter = painterResource(id = R.drawable.ic_edit),
-            contentDescription = "Icon edit"
+            painter = painterResource(id = R.drawable.ic_edit), contentDescription = "Icon edit"
         )
         Text(
             text = "Empty routine",
@@ -142,5 +153,169 @@ private fun ColumnScope.EmptyScreen() {
             style = MaterialTheme.typography.bodyMedium,
             textAlign = TextAlign.Center
         )
+    }
+}
+
+@Composable
+private fun TrainedExerciseRegion(
+    modifier: Modifier = Modifier, trainedExercise: TrainedExercise
+) {
+    val headerTitles = mutableListOf<String>()
+    when (trainedExercise.trainingSets.first()) {
+        is TrainingMeasurementMetrics.DistanceAndDuration -> {
+            headerTitles.addAll(listOf("Kilometers", "Hours"))
+        }
+        is TrainingMeasurementMetrics.DurationOnly -> {
+            headerTitles.addAll(listOf("Secs"))
+        }
+        is TrainingMeasurementMetrics.WeightAndRep -> {
+            headerTitles.addAll(listOf("Kg", "Reps"))
+        }
+    }
+
+    Column(modifier = modifier) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = dimensionResource(id = R.dimen.padding_small)),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = trainedExercise.exercise.name,
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.primary
+            )
+            Spacer(Modifier.weight(1f))
+
+            Icon(modifier = Modifier
+                .clickable {
+                    //TODO
+                }
+                .size(dimensionResource(id = R.dimen.icon_size_medium)),
+                painter = painterResource(id = R.drawable.ic_more_horizontal),
+                contentDescription = "Icon more",
+                tint = MaterialTheme.colorScheme.primary)
+        }
+
+        Column(
+            modifier = Modifier
+                .background(Color.Gray)
+                .fillMaxSize()
+                .padding(start = 8.dp, end = 8.dp), verticalArrangement = Arrangement.Center
+        ) {
+            var firstColumnWidth by remember { mutableStateOf(0) }
+            TrainingSetRow(
+                firstColumnText = "Set",
+                firstColumnWidth = firstColumnWidth,
+                onFirstColumnWidthChange = {
+                    if (it > firstColumnWidth) {
+                        firstColumnWidth = it
+                    }
+                },
+                remainingCells = headerTitles.map<String, @Composable (Modifier) -> Unit> { title ->
+                    { modifier ->
+                        Text(
+                            modifier = modifier,
+                            text = title,
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                }
+            )
+        }
+
+
+        Button(modifier = Modifier.align(Alignment.CenterHorizontally),
+            shape = RoundedCornerShape(8.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary),
+            contentPadding = PaddingValues(
+                vertical = dimensionResource(id = R.dimen.padding_small), horizontal = 0.dp
+            ),
+            onClick = { /*TODO*/ }) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_chevron_right),
+                contentDescription = "Icon arrow right",
+                tint = Color.White
+            )
+        }
+    }
+}
+
+
+/**
+ * Used together with an outer Column to create a table layout, this function can draw a row with the first columns having the same width, while the remaining columns divide the remaining width between
+ * themselves
+ *
+ * @param remainingCells The list of composables that make up the remaining cells in the row. A Modifier parameter will be provided by the function to the "remainingCells" composables to divide
+ * the remaining width between them, any composables passed to this function should use this Modifier so
+ * the function can properly calculate their width
+ *
+ * Example usage:
+ *
+ * ~~~
+ *
+ *   var firstColumnWidth by remember { mutableStateOf(0) }
+ *
+ *   TrainingSetRow(
+ *      firstColumnText = "Set",
+ *      firstColumnWidth = firstColumnWidth,
+ *      onFirstColumnWidthChange = {
+ *          if (it > firstColumnWidth) {
+ *              firstColumnWidth = it
+ *          }
+ *      },
+ *      remainingCells = listOf(
+ *          { modifier -> Text(modifier = modifier, text = "test") },
+ *          { modifier -> Text(modifier = modifier, text = "test") })
+ *      )
+ *
+ */
+@Suppress("SameParameterValue")
+@Composable
+private fun TrainingSetRow(
+    firstColumnText: String,
+    firstColumnWidth: Int,
+    onFirstColumnWidthChange: (Int) -> Unit,
+    remainingCells: List<@Composable (Modifier) -> Unit>
+) {
+
+    val widthInDp = with(LocalDensity.current) {
+        firstColumnWidth.toDp()
+    }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        if (widthInDp == 0.dp) {
+            Text(
+                text = firstColumnText,
+                modifier = Modifier
+                    .padding(horizontal = dimensionResource(id = R.dimen.padding_medium))
+                    .background(Color.Green)
+                    .onGloballyPositioned {
+                        onFirstColumnWidthChange(it.size.width)
+                    },
+                textAlign = TextAlign.Center
+            )
+        } else {
+            Text(
+                text = firstColumnText,
+                modifier = Modifier
+                    .padding(horizontal = dimensionResource(id = R.dimen.padding_medium))
+                    .background(Color.Green)
+                    .width(width = widthInDp),
+                textAlign = TextAlign.Center
+            )
+        }
+
+        remainingCells.forEach {
+            it(
+                Modifier
+                    .weight(1f)
+                    .padding(horizontal = dimensionResource(id = R.dimen.padding_small))
+                    .background(Color.Blue),
+            )
+        }
     }
 }
