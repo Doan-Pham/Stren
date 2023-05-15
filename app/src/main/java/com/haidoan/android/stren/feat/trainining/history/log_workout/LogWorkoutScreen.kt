c6bf923a7073561f21c622d17d64c6b170dd4e0b
DoanPham <doannphamm57@gmail.com>
2023-05-15 16:42:49 +0700

    feat: Add navigation info for LogWorkoutScreen

---
+++ app/src/main/java/com/haidoan/android/stren/feat/trainining/history/log_workout/LogWorkoutScreen.kt
@@ -0,0 +1,5 @@
+package com.haidoan.android.stren.feat.trainining.history.log_workout
+
+internal const val LOG_WORKOUT_SCREEN_ROUTE = "LOG_WORKOUT_SCREEN_ROUTE"
+internal const val USER_ID_WORKOUT_NAV_ARG = "USER_ID_WORKOUT_NAV_ARG"
+internal const val SELECTED_DATE_WORKOUT_NAV_ARG = "SELECTED_DATE_WORKOUT_NAV_ARG"
