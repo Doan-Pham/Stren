188ff46ae39c36a8a9010f669a5931799162c6c8
DoanPham <doannphamm57@gmail.com>
2023-05-20 22:29:22 +0700

    feat: Add data parsing for API models

--- app/src/main/java/com/haidoan/android/stren/core/network/model/NetworkFood.kt
+++ app/src/main/java/com/haidoan/android/stren/core/network/model/NetworkFood.kt
@@ -2,6 +2,7 @@ package com.haidoan.android.stren.core.network.model
 
 import com.haidoan.android.stren.core.model.Food
 import com.haidoan.android.stren.core.model.FoodNutrient
+import com.haidoan.android.stren.core.model.FoodNutrient.Companion.undefinedFoodNutrient
 import com.haidoan.android.stren.core.utils.StringFormatUtils.capitalizeFirstChar
 
 
@@ -12,15 +13,67 @@ data class NetworkFood(
     val foodNutrients: List<NetworkFoodNutrient> = listOf()
 )
 
-@kotlinx.serialization.Serializable
-data class NetworkFoodNutrient(val name: String, val amount: Float, val unitName: String)
+/**
+ * These are the names of the core nutrients as per the FDC API Specs, should not
+ * be changed unless the API Specs itself changes, or else compatibility issues
+ * will happen
+ */
+private val coreNutrients =
+    listOf("Protein", "Total lipid (fat)", "Carbohydrate, by difference")
+private const val caloriesApiField = "Energy"
 
 fun NetworkFood.asExternalModel() =
     Food(
         id = fdcId.toString(),
         name = description.capitalizeFirstChar(),
-        nutrients = foodNutrients.map { it.asExternalModel() })
+        calories = foodNutrients
+            .firstOrNull { it.name == caloriesApiField }?.asExternalModel()
+            ?: undefinedFoodNutrient,
+        coreNutrients = foodNutrients
+            .filter { it.name in coreNutrients }
+            .map { it.asExternalModel() },
+        otherNutrients = foodNutrients
+            .filterNot { it.name in coreNutrients && it.name != caloriesApiField }
+            .map { it.asExternalModel() })
 
-fun NetworkFoodNutrient.asExternalModel() =
-    FoodNutrient(name = name, amount = amount, unitName)
+
+@kotlinx.serialization.Serializable
+data class NetworkFoodNutrient(val name: String, val amount: Float, val unitName: String)
+
+/**
+ * Nutrient name taken straight from the FDC API is quite lengthy, so this helps
+ * shortening them
+ */
+private val nutrientNameMapping = mapOf(
+    "Total lipid (fat)" to "Fat",
+    "Carbohydrate" to "Carb",
+    "Energy" to "Calories"
+)
+
+private val nutrientToShowFullName =
+    listOf("Fatty acids, total saturated", "Fatty acids, total trans")
+
+fun NetworkFoodNutrient.asExternalModel(): FoodNutrient {
+    // Nutrient name from API can be lengthy (ex: it may contain both the nutrient name, its
+    // abbreviation, and other things, separated by commas)
+    var nutrientName =
+        if (name in nutrientToShowFullName) name
+        else name.substringBefore(",")
+
+    // Certain nutrients need special mapping outside of the above processing
+    nutrientName = nutrientNameMapping[nutrientName] ?: nutrientName
+
+    // Nutrients' measurement units taken from API are uppercase by default, which we don't want
+    // so this handles that case
+    val unitName =
+        if (unitName.equals("iu", true)) unitName.uppercase()
+        else unitName.lowercase()
+
+    return FoodNutrient(
+        nutrientName = nutrientName,
+        amount = amount,
+        unitName = unitName
+    )
+}
+
 
