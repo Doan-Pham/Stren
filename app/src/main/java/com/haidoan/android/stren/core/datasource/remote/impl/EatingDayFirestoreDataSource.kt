bb15f7ae241fff52e35a4e7f0e14bb128ef5f0ca
DoanPham <doannphamm57@gmail.com>
2023-05-24 22:56:29 +0700

    fix: Same meals are shown for all dates
    Cause: DataSource didn't query based on the selected date

--- app/src/main/java/com/haidoan/android/stren/core/datasource/remote/impl/EatingDayFirestoreDataSource.kt
+++ app/src/main/java/com/haidoan/android/stren/core/datasource/remote/impl/EatingDayFirestoreDataSource.kt
@@ -6,6 +6,8 @@ import com.haidoan.android.stren.core.datasource.remote.base.EatingDayRemoteData
 import com.haidoan.android.stren.core.datasource.remote.model.FirestoreEatingDay
 import com.haidoan.android.stren.core.datasource.remote.model.toExternalModel
 import com.haidoan.android.stren.core.model.EatingDay
+import com.haidoan.android.stren.core.utils.DateUtils.toTimeStampDayEnd
+import com.haidoan.android.stren.core.utils.DateUtils.toTimeStampDayStart
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.mapNotNull
 import java.time.LocalDate
@@ -19,6 +21,11 @@ class EatingDayFirestoreDataSource @Inject constructor() : EatingDayRemoteDataSo
 
     override fun getEatingDayStream(userId: String, date: LocalDate): Flow<EatingDay> =
         firestore.collection("$USER_COLLECTION_PATH/$userId/$EATING_DAY_COLLECTION_PATH")
+            .whereGreaterThanOrEqualTo("date", date.toTimeStampDayStart())
+            .whereLessThanOrEqualTo("date", date.toTimeStampDayEnd())
             .snapshots()
-            .mapNotNull { it.toObjects(FirestoreEatingDay::class.java).first().toExternalModel() }
+            .mapNotNull {
+                it.toObjects(FirestoreEatingDay::class.java).firstOrNull()?.toExternalModel()
+                    ?: EatingDay(date = date)
+            }
 }
