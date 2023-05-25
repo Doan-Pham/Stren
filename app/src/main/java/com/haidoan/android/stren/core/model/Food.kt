6a913c3ef58f4c6825e6056c19ea7f92e26d40a0
DoanPham <doannphamm57@gmail.com>
2023-05-25 14:18:23 +0700

    feat: Add method to calculate nutrient amount
    The nutrient amount in FoodNutrient class, by default, is given for
    100g of food. A method has been added to calculate this amount based
    on the actual amount of food in gram

--- app/src/main/java/com/haidoan/android/stren/core/model/Food.kt
+++ app/src/main/java/com/haidoan/android/stren/core/model/Food.kt
@@ -1,6 +1,8 @@
 package com.haidoan.android.stren.core.model
 
 import com.haidoan.android.stren.core.model.FoodNutrient.Companion.undefinedFoodNutrient
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
 data class Food(
     val id: String = "Undefined",
@@ -21,12 +23,27 @@ data class Food(
     }
 }
 
+
 data class FoodNutrient(
     val nutrientName: String = "Undefined",
     val amount: Float = -1f,
     val unitName: String = "Undefined"
 ) {
     companion object {
+        /**
+         * By default, [FoodNutrient] has its amount calculated per 100gram of food
+         */
+        const val DEFAULT_FOOD_AMOUNT_IN_GRAM = 100f
+
         val undefinedFoodNutrient = FoodNutrient("Undefined", -1F, "Undefined")
+
+        fun FoodNutrient.with(foodAmountInGram: Float): FoodNutrient {
+            val df = DecimalFormat("#.#")
+            df.roundingMode = RoundingMode.DOWN
+            val newAmount =
+                df.format(amount * foodAmountInGram / DEFAULT_FOOD_AMOUNT_IN_GRAM)
+                    .toFloat()
+            return this.copy(amount = newAmount)
+        }
     }
 }
