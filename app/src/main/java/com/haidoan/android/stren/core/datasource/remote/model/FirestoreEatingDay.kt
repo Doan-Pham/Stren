4473805598c751c7d1cdbfa9518cc4f404c7feb1
DoanPham <doannphamm57@gmail.com>
2023-05-25 22:41:08 +0700

    feat: Implement adding new eating day

--- app/src/main/java/com/haidoan/android/stren/core/datasource/remote/model/FirestoreEatingDay.kt
+++ app/src/main/java/com/haidoan/android/stren/core/datasource/remote/model/FirestoreEatingDay.kt
@@ -5,13 +5,23 @@ import com.google.firebase.firestore.DocumentId
 import com.haidoan.android.stren.core.model.EatingDay
 import com.haidoan.android.stren.core.model.Meal
 import com.haidoan.android.stren.core.utils.DateUtils.toLocalDate
+import com.haidoan.android.stren.core.utils.DateUtils.toTimeStampDayStart
 
 internal data class FirestoreEatingDay(
     @DocumentId
     val id: String = "Undefined EatingDay ID",
     val date: Timestamp = Timestamp.now(),
     val meals: List<Meal> = listOf()
-)
+) {
+    companion object {
+        internal fun from(eatingDay: EatingDay): FirestoreEatingDay =
+            FirestoreEatingDay(
+                id = eatingDay.id,
+                date = eatingDay.date.toTimeStampDayStart(),
+                meals = eatingDay.meals
+            )
+    }
+}
 
 internal fun FirestoreEatingDay.toExternalModel(): EatingDay =
     EatingDay(id = id, date = date.toLocalDate(), meals = meals)
