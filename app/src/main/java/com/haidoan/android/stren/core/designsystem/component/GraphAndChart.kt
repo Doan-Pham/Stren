92ee55b134abfc8a1d3a5a4cba423af5958ebc58
DoanPham <doannphamm57@gmail.com>
2023-05-27 17:23:15 +0700

    feat: Implement calories line chart

--- app/src/main/java/com/haidoan/android/stren/core/designsystem/component/GraphAndChart.kt
+++ app/src/main/java/com/haidoan/android/stren/core/designsystem/component/GraphAndChart.kt
@@ -1,14 +1,18 @@
 package com.haidoan.android.stren.core.designsystem.component
 
+import android.graphics.Typeface
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -23,6 +27,32 @@ import com.github.tehras.charts.piechart.animation.simpleChartAnimation
 import com.github.tehras.charts.piechart.renderer.SimpleSliceDrawer
 import com.haidoan.android.stren.R
 import com.haidoan.android.stren.core.designsystem.theme.*
+import com.patrykandpatrick.vico.compose.axis.horizontal.bottomAxis
+import com.patrykandpatrick.vico.compose.axis.vertical.startAxis
+import com.patrykandpatrick.vico.compose.chart.Chart
+import com.patrykandpatrick.vico.compose.chart.line.lineChart
+import com.patrykandpatrick.vico.compose.component.lineComponent
+import com.patrykandpatrick.vico.compose.component.overlayingComponent
+import com.patrykandpatrick.vico.compose.component.shapeComponent
+import com.patrykandpatrick.vico.compose.component.textComponent
+import com.patrykandpatrick.vico.compose.dimensions.dimensionsOf
+import com.patrykandpatrick.vico.core.axis.AxisPosition
+import com.patrykandpatrick.vico.core.axis.formatter.AxisValueFormatter
+import com.patrykandpatrick.vico.core.axis.horizontal.HorizontalAxis
+import com.patrykandpatrick.vico.core.chart.insets.Insets
+import com.patrykandpatrick.vico.core.chart.segment.SegmentProperties
+import com.patrykandpatrick.vico.core.component.marker.MarkerComponent
+import com.patrykandpatrick.vico.core.component.shape.DashedShape
+import com.patrykandpatrick.vico.core.component.shape.ShapeComponent
+import com.patrykandpatrick.vico.core.component.shape.Shapes
+import com.patrykandpatrick.vico.core.component.shape.cornered.Corner
+import com.patrykandpatrick.vico.core.component.shape.cornered.MarkerCorneredShape
+import com.patrykandpatrick.vico.core.context.MeasureContext
+import com.patrykandpatrick.vico.core.entry.ChartEntry
+import com.patrykandpatrick.vico.core.entry.ChartEntryModelProducer
+import com.patrykandpatrick.vico.core.extension.copyColor
+import com.patrykandpatrick.vico.core.marker.Marker
+import java.time.LocalDate
 
 @Composable
 fun PieChartWithLegend(
@@ -160,3 +190,143 @@ enum class PieChartSize(val value: Dp) {
     MEDIUM(140.dp),
     DEFAULT(150.dp)
 }
+
+
+@Composable
+fun StrenLineChart(
+    modifier: Modifier = Modifier,
+    chartEntryModelProducer: ChartEntryModelProducer
+) {
+    val axisValueFormatter =
+        AxisValueFormatter<AxisPosition.Horizontal.Bottom> { value, chartValues ->
+            (chartValues.chartEntryModel.entries.first()
+                .getOrNull(value.toInt()) as? DateChartEntry)
+                ?.localDate
+                ?.run { "$dayOfMonth/$monthValue" }
+                .orEmpty()
+        }
+    val marker = rememberMarker()
+    Chart(
+        modifier = modifier,
+        marker = marker,
+        chart = lineChart(),
+        chartModelProducer = chartEntryModelProducer,
+        startAxis = startAxis(),
+        bottomAxis = bottomAxis(
+            guideline = lineComponent(thickness = 0.dp),
+            valueFormatter = axisValueFormatter,
+            tickPosition = HorizontalAxis.TickPosition.Edge
+        ),
+    )
+}
+
+fun List<Pair<LocalDate, Float>>.toCharEntryModelProducer() = this.mapIndexed { index, (date, y) ->
+    DateChartEntry(
+        date,
+        index.toFloat(),
+        y
+    )
+}.let { ChartEntryModelProducer(it) }
+
+fun List<Pair<LocalDate, Float>>.toCharEntries() = this.mapIndexed { index, (date, y) ->
+    DateChartEntry(
+        date,
+        index.toFloat(),
+        y
+    )
+}
+
+class DateChartEntry(
+    val localDate: LocalDate,
+    override val x: Float,
+    override val y: Float,
+) : ChartEntry {
+    override fun withY(y: Float) = DateChartEntry(localDate, x, y)
+}
+
+@Composable
+private fun rememberMarker(): Marker {
+    val labelBackgroundColor = MaterialTheme.colorScheme.surface
+    val labelBackground = remember(labelBackgroundColor) {
+        ShapeComponent(labelBackgroundShape, labelBackgroundColor.toArgb()).setShadow(
+            radius = LABEL_BACKGROUND_SHADOW_RADIUS,
+            dy = LABEL_BACKGROUND_SHADOW_DY,
+            applyElevationOverlay = true,
+        )
+    }
+    val label = textComponent(
+        background = labelBackground,
+        lineCount = LABEL_LINE_COUNT,
+        padding = labelPadding,
+        typeface = Typeface.MONOSPACE,
+    )
+    val indicatorInnerComponent =
+        shapeComponent(Shapes.pillShape, MaterialTheme.colorScheme.surface)
+    val indicatorCenterComponent = shapeComponent(Shapes.pillShape, Color.White)
+    val indicatorOuterComponent = shapeComponent(Shapes.pillShape, Color.White)
+    val indicator = overlayingComponent(
+        outer = indicatorOuterComponent,
+        inner = overlayingComponent(
+            outer = indicatorCenterComponent,
+            inner = indicatorInnerComponent,
+            innerPaddingAll = indicatorInnerAndCenterComponentPaddingValue,
+        ),
+        innerPaddingAll = indicatorCenterAndOuterComponentPaddingValue,
+    )
+    val guideline = lineComponent(
+        MaterialTheme.colorScheme.onSurface.copy(GUIDELINE_ALPHA),
+        guidelineThickness,
+        guidelineShape,
+    )
+    return remember(label, indicator, guideline) {
+        object : MarkerComponent(label, indicator, guideline) {
+            init {
+                indicatorSizeDp = INDICATOR_SIZE_DP
+                onApplyEntryColor = { entryColor ->
+                    indicatorOuterComponent.color =
+                        entryColor.copyColor(INDICATOR_OUTER_COMPONENT_ALPHA)
+                    with(indicatorCenterComponent) {
+                        color = entryColor
+                        setShadow(
+                            radius = INDICATOR_CENTER_COMPONENT_SHADOW_RADIUS,
+                            color = entryColor
+                        )
+                    }
+                }
+            }
+
+            override fun getInsets(
+                context: MeasureContext,
+                outInsets: Insets,
+                segmentProperties: SegmentProperties
+            ) =
+                with(context) {
+                    outInsets.top =
+                        label.getHeight(context) + labelBackgroundShape.tickSizeDp.pixels +
+                                LABEL_BACKGROUND_SHADOW_RADIUS.pixels * SHADOW_RADIUS_MULTIPLIER -
+                                LABEL_BACKGROUND_SHADOW_DY.pixels
+                }
+        }
+    }
+}
+
+private const val LABEL_BACKGROUND_SHADOW_RADIUS = 4f
+private const val LABEL_BACKGROUND_SHADOW_DY = 2f
+private const val LABEL_LINE_COUNT = 1
+private const val GUIDELINE_ALPHA = .2f
+private const val INDICATOR_SIZE_DP = 36f
+private const val INDICATOR_OUTER_COMPONENT_ALPHA = 32
+private const val INDICATOR_CENTER_COMPONENT_SHADOW_RADIUS = 12f
+private const val GUIDELINE_DASH_LENGTH_DP = 8f
+private const val GUIDELINE_GAP_LENGTH_DP = 4f
+private const val SHADOW_RADIUS_MULTIPLIER = 1.3f
+
+private val labelBackgroundShape = MarkerCorneredShape(Corner.FullyRounded)
+private val labelHorizontalPaddingValue = 8.dp
+private val labelVerticalPaddingValue = 4.dp
+private val labelPadding = dimensionsOf(labelHorizontalPaddingValue, labelVerticalPaddingValue)
+private val indicatorInnerAndCenterComponentPaddingValue = 5.dp
+private val indicatorCenterAndOuterComponentPaddingValue = 10.dp
+private val guidelineThickness = 2.dp
+private val guidelineShape =
+    DashedShape(Shapes.pillShape, GUIDELINE_DASH_LENGTH_DP, GUIDELINE_GAP_LENGTH_DP)
--- app/src/main/java/com/haidoan/android/stren/feat/dashboard/DashboardNavigation.kt
+++ app/src/main/java/com/haidoan/android/stren/feat/dashboard/DashboardNavigation.kt
@@ -1,16 +1,12 @@
 package com.haidoan.android.stren.feat.dashboard
 
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import com.google.accompanist.navigation.animation.composable
 import com.google.accompanist.navigation.animation.navigation
 import com.haidoan.android.stren.app.navigation.AppBarConfiguration
-import com.haidoan.android.stren.core.designsystem.component.DummyBoxWithText
 
 const val DASHBOARD_GRAPH_ROUTE = "dashboard_graph_route"
 const val DASHBOARD_GRAPH_STARTING_ROUTE = "dashboard_graph_starting_route"
@@ -22,12 +18,9 @@ fun NavGraphBuilder.dashboardGraph(
 ) {
     navigation(startDestination = DASHBOARD_GRAPH_STARTING_ROUTE, route = DASHBOARD_GRAPH_ROUTE) {
         composable(route = DASHBOARD_GRAPH_STARTING_ROUTE) {
-            DummyBoxWithText(text = "Dashboard")
-            var isAppBarConfigured by remember { mutableStateOf(false) }
-            if (!isAppBarConfigured) {
-                appBarConfigurationChangeHandler(AppBarConfiguration.NavigationAppBar())
-                isAppBarConfigured = true
-            }
+            DashboardRoute(appBarConfigurationChangeHandler = {
+                appBarConfigurationChangeHandler(it)
+            })
         }
     }
 }
