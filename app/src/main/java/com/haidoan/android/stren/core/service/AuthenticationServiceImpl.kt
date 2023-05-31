7ac910893216b14cffd8edd04db0d5d3571a9a44
DoanPham <doannphamm57@gmail.com>
2023-05-31 22:27:02 +0700

    feat: Modify methods of AuthenticationService
    Including:
    - Add getUserAuthStateStream() which returns an auto-updated Flow
    of user auth state. The addAuthStateListeners() which was the callback
    version of this method worked well for plain callbacks, but falls short
    when developers need to call suspend functions in these callbacks.
    getUserAuthStateStream() returns a Flow so the above shortcoming doesn't
    exist
    - Modify authenticate() method (which basically signs user in) so it
    only succeeds if the email being authenticated is already verified by
    user, or else an exception is thrown for the UI layer to handle.
    Without the above logic, Firestore will just signs users in without
    a care for whether users have their emails verified or just use
    some random emails
    - Add signOut() method for signing out

--- app/src/main/java/com/haidoan/android/stren/core/service/AuthenticationServiceImpl.kt
+++ app/src/main/java/com/haidoan/android/stren/core/service/AuthenticationServiceImpl.kt
@@ -2,20 +2,24 @@ package com.haidoan.android.stren.core.service
 
 import com.facebook.AccessToken
 import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.haidoan.android.stren.core.model.User
+import com.haidoan.android.stren.core.service.model.UserAuthState
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.tasks.await
 import timber.log.Timber
 import javax.inject.Inject
 
 class AuthenticationServiceImpl @Inject constructor() : AuthenticationService {
-
     override fun addAuthStateListeners(
         onUserAuthenticated: (userId: String) -> Unit,
         onUserNotAuthenticated: () -> Unit
     ) {
-        //Firebase.auth.signOut()
         Firebase.auth.addAuthStateListener {
             Timber.d("addAuthStateListeners() - currentUser: ${it.currentUser}")
             if (it.currentUser != null) {
@@ -26,13 +30,50 @@ class AuthenticationServiceImpl @Inject constructor() : AuthenticationService {
         }
     }
 
+    override fun getUserAuthStateStream(): Flow<UserAuthState?> = callbackFlow {
+        val authStateListener = FirebaseAuth.AuthStateListener {
+            try {
+                Timber.d("getUserAuthIdFlow() - Flow emits new value: ${it.currentUser}")
+                if (it.currentUser != null) {
+                    trySend(
+                        UserAuthState(
+                            user = User(
+                                id = it.currentUser!!.uid,
+                                email = it.currentUser!!.email ?: "NOT_USE_EMAIL"
+                            ),
+                            isUserSigningUp = !it.currentUser!!.isEmailVerified
+                        )
+                    ).isSuccess
+                } else {
+                    trySend(null)
+                }
+            } catch (e: Throwable) {
+                Timber.d("getUserAuthIdFlow() - Exception: $e")
+            }
+        }
+        Firebase.auth.addAuthStateListener(authStateListener)
+        Timber.d("getUserAuthIdFlow() - AuthStateListener has been added")
+
+        awaitClose {
+            Firebase.auth.removeAuthStateListener(authStateListener)
+            Timber.d("getUserAuthIdFlow() - AuthStateListener has been removed")
+        }
+    }
+
+    /**
+     * This method may throw an exception if the [email] is not verified
+     */
     override suspend fun authenticate(email: String, password: String) {
         Firebase.auth.signInWithEmailAndPassword(email, password).await()
+        if (Firebase.auth.currentUser?.isEmailVerified == false) {
+            throw UnverifiedEmailException(email)
+        }
+        Firebase.auth.currentUser?.reload()
     }
 
     override suspend fun signUp(email: String, password: String) {
         Firebase.auth.createUserWithEmailAndPassword(email, password).await()
-        Firebase.auth.signOut()
+        Firebase.auth.currentUser?.sendEmailVerification()?.await()
     }
 
     override suspend fun authenticateWithFacebook(token: AccessToken) {
@@ -43,5 +84,9 @@ class AuthenticationServiceImpl @Inject constructor() : AuthenticationService {
     override suspend fun authenticateWithGoogle(tokenId: String) {
         val firebaseCredential = GoogleAuthProvider.getCredential(tokenId, null)
         Firebase.auth.signInWithCredential(firebaseCredential).await()
+    }
+
+    override fun signOut() {
+        Firebase.auth.signOut()
     }
 }
