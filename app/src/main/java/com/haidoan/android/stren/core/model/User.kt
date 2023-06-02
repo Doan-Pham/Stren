21839addb7888c2f2db7fc74e34d3bf2aaef5d16
DoanPham <doannphamm57@gmail.com>
2023-06-02 11:47:11 +0700

    feat: Add secondary model classes to User.kt
    These mostly have meaning only when coupled to a User so they are
    added in User.kt

--- app/src/main/java/com/haidoan/android/stren/core/model/User.kt
+++ app/src/main/java/com/haidoan/android/stren/core/model/User.kt
@@ -10,12 +10,96 @@ data class User(
     val id: String,
     val email: String,
     val shouldShowOnboarding: Boolean,
+    val biometricsRecords: List<BiometricsRecord> = listOf(),
+    val goals: List<Goal> = listOf(),
     val trackedCategories: List<TrackedCategory> = listOf()
 ) {
     companion object {
-        val undefined = User("Undefined", "Undefined", true)
+        val undefined = User("Undefined", "Undefined", true, listOf())
     }
 }
+
+enum class CommonBiometricsIds {
+    BIOMETRICS_ID_WEIGHT,
+    BIOMETRICS_ID_HEIGHT
+}
+
+data class BiometricsRecord(
+    @DocumentId
+    val id: String = "Undefined Document Id",
+    val biometricsId: String,
+    val recordDate: LocalDate,
+    val measurementUnit: String,
+    val value: Float
+) {
+    companion object {
+        fun createWeightBiometrics(weightInKg: Float, date: LocalDate) =
+            BiometricsRecord(
+                biometricsId = CommonBiometricsIds.BIOMETRICS_ID_WEIGHT.toString(),
+                recordDate = date,
+                measurementUnit = "kg",
+                value = weightInKg
+            )
+
+        fun createHeightBiometrics(heightInCm: Float, date: LocalDate) =
+            BiometricsRecord(
+                biometricsId = CommonBiometricsIds.BIOMETRICS_ID_HEIGHT.toString(),
+                recordDate = date,
+                measurementUnit = "cm",
+                value = heightInCm
+            )
+    }
+
+}
+
+const val GOAL_ID_CALORIES = "GOAL_ID_CALORIES"
+private fun createCalorieGoal(amountInKcal: Float) =
+    Goal.FoodNutrientGoal(
+        id = GOAL_ID_CALORIES,
+        name = "Calories",
+        type = GoalType.FOOD_NUTRIENT,
+        value = amountInKcal,
+        foodNutrientId = FOOD_NUTRIENT_ID_CALORIES
+    )
+
+fun CoreNutrient.toGoal(nutrientAmountInGram: Float) = Goal.FoodNutrientGoal(
+    id = "GOAL_ID" + "_" + this.id,
+    name = this.nutrientName,
+    type = GoalType.FOOD_NUTRIENT,
+    value = nutrientAmountInGram,
+    foodNutrientId = this.id
+)
+
+enum class GoalType {
+    FOOD_NUTRIENT
+}
+
+sealed class Goal {
+    abstract val id: String
+    abstract val name: String
+    abstract val type: GoalType
+    abstract val value: Float
+
+    data class FoodNutrientGoal(
+        override val id: String,
+        override val name: String,
+        override val type: GoalType = GoalType.FOOD_NUTRIENT,
+        override val value: Float,
+        val foodNutrientId: String
+    ) : Goal() {
+        companion object {
+            fun createCoreNutrientAndCalorieGoals(
+                coreNutrientAmountInGram: Map<CoreNutrient, Float>,
+                caloriesAmountInKcal: Float
+            ) = coreNutrientAmountInGram.keys.map {
+                it.toGoal(
+                    coreNutrientAmountInGram[it] ?: 0f
+                )
+            } + createCalorieGoal(caloriesAmountInKcal)
+        }
+    }
+}
+
 
 sealed class TrackedCategory {
     @get:PropertyName("isDefaultCategory")
