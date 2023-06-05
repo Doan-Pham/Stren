f2dfb0d014bf2c47474fb3d7a3ee65401a817888
DoanPham <doannphamm57@gmail.com>
2023-06-05 22:05:24 +0700

    feat: Implement ExercisesRepositoryImpl
    This class uses 2 remote DataSources, 1 specialized in searching
    (which uses Algolia behind-the-scenes for full-text search) and 1 for
    other data-manipulation operations

--- app/src/main/java/com/haidoan/android/stren/core/repository/base/ExercisesRepository.kt
+++ app/src/main/java/com/haidoan/android/stren/core/repository/base/ExercisesRepository.kt
@@ -3,25 +3,25 @@ package com.haidoan.android.stren.core.repository.base
 import androidx.paging.PagingData
 import com.haidoan.android.stren.core.model.Exercise
 import com.haidoan.android.stren.core.model.ExerciseCategory
-import com.haidoan.android.stren.core.model.ExerciseFilterStandards
+import com.haidoan.android.stren.core.model.ExerciseQueryParameters
 import com.haidoan.android.stren.core.model.MuscleGroup
 import kotlinx.coroutines.flow.Flow
 
-const val DEFAULT_ITEM_COUNT_LIMIT = 20L
+const val DEFAULT_EXERCISE_DATA_PAGE_SIZE = 20L
 
 interface ExercisesRepository {
-    fun getExercisesWithLimit(limit: Long = DEFAULT_ITEM_COUNT_LIMIT): Flow<PagingData<Exercise>>
+    fun getExercisesWithLimit(limit: Long = DEFAULT_EXERCISE_DATA_PAGE_SIZE): Flow<PagingData<Exercise>>
     fun getExercisesByNameWithLimit(
         exerciseName: String,
-        limit: Long = DEFAULT_ITEM_COUNT_LIMIT
+        limit: Long = DEFAULT_EXERCISE_DATA_PAGE_SIZE
     ): Flow<PagingData<Exercise>>
 
     fun getAllExerciseCategories(): Flow<List<ExerciseCategory>>
     fun getAllMuscleGroups(): Flow<List<MuscleGroup>>
 
-    fun filterExercises(
-        filterStandards: ExerciseFilterStandards,
-        resultCountLimit: Long = DEFAULT_ITEM_COUNT_LIMIT
+    fun searchExercises(
+        filterStandards: ExerciseQueryParameters,
+        resultCountLimit: Long = DEFAULT_EXERCISE_DATA_PAGE_SIZE
     ): Flow<PagingData<Exercise>>
 
     fun getExerciseById(exerciseId: String): Flow<Exercise>
