11973d2670a82bf08d00490c98375cbcf5148d3b
DoanPham <doannphamm57@gmail.com>
2023-06-07 13:38:14 +0700

    feat: Add methods for uploading image files

--- app/src/main/java/com/haidoan/android/stren/core/service/di/ServiceModule.kt
+++ app/src/main/java/com/haidoan/android/stren/core/service/di/ServiceModule.kt
@@ -2,6 +2,8 @@ package com.haidoan.android.stren.core.service.di
 
 import com.haidoan.android.stren.core.service.AuthenticationService
 import com.haidoan.android.stren.core.service.AuthenticationServiceImpl
+import com.haidoan.android.stren.core.service.StorageService
+import com.haidoan.android.stren.core.service.StorageServiceFirebaseImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -12,4 +14,7 @@ import dagger.hilt.android.components.ViewModelComponent
 abstract class ServiceModule {
     @Binds
     abstract fun provideAuthService(impl: AuthenticationServiceImpl): AuthenticationService
+
+    @Binds
+    abstract fun provideStorageService(impl: StorageServiceFirebaseImpl): StorageService
 }
