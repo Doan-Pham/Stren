34b8f0a5670407809c64097ebf91d8502db05eb2
DoanPham <doannphamm57@gmail.com>
2023-06-21 14:00:28 +0700

    feat: Simplify StartWorkoutViewModel
    Since most of StartWorkoutScreen's data and business logic is hoisted
    up in the WorkoutInProgressViewModel (see #1e41bd22 for more details),
    StartWorkoutViewModel now only has to handle some simple UI
    state-related operations

--- app/src/main/java/com/haidoan/android/stren/feat/training/history/start_workout/StartWorkoutViewModel.kt
+++ app/src/main/java/com/haidoan/android/stren/feat/training/history/start_workout/StartWorkoutViewModel.kt
@@ -7,15 +7,10 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.haidoan.android.stren.core.model.*
-import com.haidoan.android.stren.core.repository.base.ExercisesRepository
-import com.haidoan.android.stren.core.repository.base.RoutinesRepository
-import com.haidoan.android.stren.core.repository.base.WorkoutsRepository
 import com.haidoan.android.stren.feat.training.history.StartWorkoutArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.launch
-import timber.log.Timber
 import javax.inject.Inject
 
 internal const val NO_SELECTION_ROUTINE_ID = "NO SELECTION ROUTINE ID"
@@ -24,58 +19,16 @@ internal const val NO_SELECTION_ROUTINE_NAME = "None"
 @HiltViewModel
 internal class StartWorkoutViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
-    private val routinesRepository: RoutinesRepository,
-    private val exercisesRepository: ExercisesRepository,
-    private val workoutsRepository: WorkoutsRepository
 ) : ViewModel() {
 
     var workoutNameTextFieldValue by mutableStateOf("New workout")
-    private lateinit var currentWorkoutInfo: Workout
 
-    private val navArgs: StartWorkoutArgs = StartWorkoutArgs(savedStateHandle)
+    val navArgs: StartWorkoutArgs = StartWorkoutArgs(savedStateHandle)
     private val _trainedExercises: MutableStateFlow<List<TrainedExercise>> =
         MutableStateFlow(listOf())
 
-    private var _currentSelectedRoutineId = NO_SELECTION_ROUTINE_ID
-    private val _routines = MutableStateFlow(listOf<Routine>())
-    val routines: StateFlow<List<Routine>> = _routines
-
     private val _secondaryUiState = MutableStateFlow(StartWorkoutSecondaryUiState())
     val secondaryUiState: StateFlow<StartWorkoutSecondaryUiState> = _secondaryUiState
-
-    init {
-        Timber.d("init() - userId: ${navArgs.userId} - selectedDate: ${navArgs.selectedDate}")
-        viewModelScope.launch {
-            val allRoutines = routinesRepository.getRoutinesByUserId(navArgs.userId).toMutableList()
-            allRoutines.add(
-                0, Routine(
-                    id = NO_SELECTION_ROUTINE_ID,
-                    name = NO_SELECTION_ROUTINE_NAME,
-                    trainedExercises = listOf()
-                )
-            )
-            _routines.value = allRoutines
-            // In case user just navigates to StartWorkoutScreen from RoutinesScreen to quickly
-            // log workout, then selectedRoutineId won't be NO_SELECTION_ROUTINE_ID
-            // at init
-            selectRoutine(navArgs.selectedRoutineId)
-
-            Timber.d("init() - selectedRoutineId: ${navArgs.selectedRoutineId}")
-            Timber.d("init() - routines: ${routines.value}")
-        }
-
-        if (!navArgs.isAddingWorkout) {
-            viewModelScope.launch {
-                val workout = workoutsRepository.getWorkoutById(navArgs.workoutId)
-                workoutNameTextFieldValue = workout.name
-                _trainedExercises.value = workout.trainedExercises
-                currentWorkoutInfo = workout
-
-                Timber.d("workout: $workout")
-                Timber.d(" _trainedExercises.value: ${_trainedExercises.value}")
-            }
-        }
-    }
 
     fun updateBackConfirmDialogState(shouldShowDialog: Boolean) {
         _secondaryUiState.update { currentState -> currentState.copy(shouldShowBackConfirmDialog = shouldShowDialog) }
@@ -87,183 +40,27 @@ internal class StartWorkoutViewModel @Inject constructor(
         }
     }
 
-    fun selectRoutine(newlySelectedRoutineId: String) {
-        if (newlySelectedRoutineId == _currentSelectedRoutineId) return
-
+    /**
+     * Since [StartWorkoutViewModel] simply holds uiState for [StartWorkoutScreen] and doesn't hold any business logic (Which is hoisted up in [WorkoutInProgressViewModel] for use across different screens), this method simply does some checks before delegating the actual routine selection to [onSelectRoutine] param
+     */
+    fun selectRoutineDropdownOption(selectedOptionRoutineId: String, onSelectRoutine: () -> Unit) {
         if (_trainedExercises.value.isEmpty()) {
-            if (newlySelectedRoutineId != NO_SELECTION_ROUTINE_ID) {
-                _currentSelectedRoutineId = newlySelectedRoutineId
-                _trainedExercises.value =
-                    routines.value.first { it.id == _currentSelectedRoutineId }.trainedExercises
-                _secondaryUiState.update { currentState ->
-                    currentState.copy(selectedRoutineId = _currentSelectedRoutineId)
-                }
+            if (selectedOptionRoutineId != NO_SELECTION_ROUTINE_ID) {
+                onSelectRoutine()
             }
         } else {
             _secondaryUiState.update { currentState ->
                 currentState.copy(
                     shouldShowRoutineWarningDialog = true,
                     onConfirmSwitchRoutine = {
-                        _currentSelectedRoutineId = newlySelectedRoutineId
-                        _trainedExercises.value =
-                            routines.value.first { it.id == _currentSelectedRoutineId }.trainedExercises
-                        _secondaryUiState.update { currentState ->
-                            currentState.copy(selectedRoutineId = _currentSelectedRoutineId)
-                        }
+                        onSelectRoutine()
                     })
             }
         }
     }
 
-    fun setExercisesIdsToAdd(ids: List<String>) {
-        //Timber.d("setExercisesIdsToAdd - ids: $ids")
-        viewModelScope.launch {
-            val exercisesToAdd = exercisesRepository.getExercisesByIds(ids)
-            //Timber.d("exercisesToAdd: $exercisesToAdd")
-
-            val currentTrainedExercises = mutableListOf<TrainedExercise>()
-            currentTrainedExercises.addAll(_trainedExercises.value)
-            currentTrainedExercises.addAll(exercisesToAdd.map { it.asTrainedExerciseWithOneSet() })
-
-            _trainedExercises.value = currentTrainedExercises
-            Timber.d("_trainedExercises.value.size: ${_trainedExercises.value.size}")
-        }
-    }
-
-    fun updateExerciseTrainingSet(
-        exerciseToUpdate: TrainedExercise,
-        trainingSetToUpdate: TrainingMeasurementMetrics,
-        updatedTrainingSetData: TrainingMeasurementMetrics
-    ) {
-        Timber.d("exerciseToUpdate - unique identifier: ${exerciseToUpdate.id}; content: $exerciseToUpdate")
-        Timber.d(
-            "trainingSetToUpdate - unique identifier: ${
-                trainingSetToUpdate.id
-            }; content: $trainingSetToUpdate"
-        )
-        Timber.d(
-            "updatedTrainingSetData - unique identifier: ${
-                updatedTrainingSetData.id
-            }; content: $updatedTrainingSetData"
-        )
-
-        val updatedTrainingSets = _trainedExercises.value.first { trainedExercise ->
-            Timber.d("Finding exercise - id: ${trainedExercise.id}")
-            trainedExercise.id == exerciseToUpdate.id
-        }.trainingSets.replace(updatedTrainingSetData) { trainingSet ->
-            Timber.d("Replacing training set - id: ${trainingSet.id}")
-            trainingSet.id == trainingSetToUpdate.id
-        }
-        Timber.d("updatedTrainingSets: $updatedTrainingSets")
-
-        val updatedExercise = _trainedExercises.value.first {
-            Timber.d("Finding exercise - id: ${it.id}")
-            it.id == exerciseToUpdate.id
-        }.copy(trainingSets = updatedTrainingSets)
-        Timber.d("updatedExercise: $updatedExercise")
-
-        val updatedTrainedExercises = mutableListOf<TrainedExercise>()
-        updatedTrainedExercises.addAll(_trainedExercises.value.replace(updatedExercise) { it.id == exerciseToUpdate.id })
-        Timber.d("updatedTrainedExercises: $updatedTrainedExercises")
-
-        _trainedExercises.value = updatedTrainedExercises
-        Timber.d("_trainedExercises: ${_trainedExercises.value}")
-    }
-
-    fun addEmptyTrainingSet(
-        exerciseToUpdate: TrainedExercise,
-    ) {
-
-//        Timber.d("addTrainingSet - exerciseToUpdate: unique identifier: ${exerciseToUpdate.id}; content: $exerciseToUpdate")
-
-        val updatedTrainingSets = _trainedExercises.value.first { trainedExercise ->
-            Timber.d("Finding exercise - id: ${trainedExercise.id}")
-            trainedExercise.id == exerciseToUpdate.id
-        }.trainingSets.toMutableList()
-        updatedTrainingSets.addEmptyTrainingSet()
-
-        val updatedExercise = _trainedExercises.value.first {
-//            Timber.d("Finding exercise - id: ${it.id}")
-            it.id == exerciseToUpdate.id
-        }.copy(trainingSets = updatedTrainingSets)
-
-        val updatedTrainedExercises = mutableListOf<TrainedExercise>()
-        updatedTrainedExercises.addAll(_trainedExercises.value.replace(updatedExercise) { it.id == exerciseToUpdate.id })
-//        Timber.d("updatedTrainedExercises: $updatedTrainedExercises")
-
-        _trainedExercises.value = updatedTrainedExercises
-//        Timber.d("_trainedExercises: ${_trainedExercises.value}")
-    }
-
-    fun deleteExercise(
-        exerciseToDelete: TrainedExercise,
-    ) {
-        val updatedTrainedExercises = _trainedExercises.value.toMutableList()
-        updatedTrainedExercises.removeIf { it.id == exerciseToDelete.id }
-
-        Timber.d("updatedTrainedExercises: $updatedTrainedExercises")
-
-        _trainedExercises.value = updatedTrainedExercises
-        Timber.d("_trainedExercises: ${_trainedExercises.value}")
-    }
-
-    fun deleteTrainingSet(
-        exerciseToUpdate: TrainedExercise,
-        trainingSetToDelete: TrainingMeasurementMetrics,
-    ) {
-        Timber.d("exerciseToUpdate - unique identifier: ${exerciseToUpdate.id}; content: $exerciseToUpdate")
-        Timber.d(
-            "trainingSetToUpdate - unique identifier: ${
-                trainingSetToDelete.id
-            }; content: $trainingSetToDelete"
-        )
-
-        val updatedTrainingSets = _trainedExercises.value.first { trainedExercise ->
-            Timber.d("Finding exercise - id: ${trainedExercise.id}")
-            trainedExercise.id == exerciseToUpdate.id
-        }.trainingSets.toMutableList()
-        updatedTrainingSets.removeIf { it.id == trainingSetToDelete.id }
-
-        Timber.d("updatedTrainingSets: $updatedTrainingSets")
-
-        val updatedExercise = _trainedExercises.value.first {
-            Timber.d("Finding exercise - id: ${it.id}")
-            it.id == exerciseToUpdate.id
-        }.copy(trainingSets = updatedTrainingSets)
-        Timber.d("updatedExercise: $updatedExercise")
-
-        val updatedTrainedExercises = mutableListOf<TrainedExercise>()
-        updatedTrainedExercises.addAll(_trainedExercises.value.replace(updatedExercise) { it.id == exerciseToUpdate.id })
-        Timber.d("updatedTrainedExercises: $updatedTrainedExercises")
-
-        _trainedExercises.value = updatedTrainedExercises
-        Timber.d("_trainedExercises: ${_trainedExercises.value}")
-    }
-
-    fun addEditWorkout() {
-        Timber.d("addEditWorkout() - userId: ${navArgs.userId}")
-        if (navArgs.isAddingWorkout) {
-            viewModelScope.launch {
-                workoutsRepository.addWorkout(
-                    userId = navArgs.userId,
-                    workout = Workout(
-                        name = workoutNameTextFieldValue,
-                        date = navArgs.selectedDate,
-                        trainedExercises = _trainedExercises.value
-                    )
-                )
-            }
-        } else {
-            viewModelScope.launch {
-                workoutsRepository.updateWorkout(
-                    userId = navArgs.userId,
-                    workout = currentWorkoutInfo.copy(
-                        name = workoutNameTextFieldValue,
-                        trainedExercises = _trainedExercises.value
-                    )
-                )
-            }
-        }
+    fun setTrainedExercises(trainedExercises: List<TrainedExercise>) {
+        _trainedExercises.update { trainedExercises }
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
@@ -276,10 +73,4 @@ internal class StartWorkoutViewModel @Inject constructor(
             viewModelScope, SharingStarted.WhileSubscribed(5000), StartWorkoutUiState.Loading
         )
 
-}
-
-private fun <T> List<T>.replace(newValue: T, block: (T) -> Boolean): List<T> {
-    return map {
-        if (block(it)) newValue else it
-    }
 }
